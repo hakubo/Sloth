5af1345c72d5e3596daf518968562ee97c8c0cf1
Jakub Olek <jolek@wikia-inc.com>
2013-02-17 12:09:25 +0100

    Work in progress: optimizations, firing callback, removing callbacks when not needed anymore

--- sloth.js
+++ sloth.js
@@ -1,42 +1,69 @@
 define('Sloth', function(){
 
 	//some private vars
-	var undef,
+	var slice = Array.prototype.slice,
+		win = window,
+		scr = win.screen,
+		undef,
+		delegate = win.setTimout,
 		branches = [],
-		Branch = function(elements, threshold, callback){
-			this.elements = elements;
+		Branch = function(element, threshold, callback){
+			this.element = element;
 			this.threshold = threshold;
-			this.callback = callback;
+			this.callback = function(){
+				callback(element);
+			};
+		},
+		execute = function(event){
+			var i = branches.length-1,
+				branch;
+			console.time('call')
+			while(branch = branches[i--]){
+				if(branch.isVisible()) {
+					console.log(branch);
+					delegate(branch.callback,0);
+					branches.splice(i, 1);
+				}
+			}
+			console.timeEnd('call')
 		};
 
+	Branch.prototype.isVisible = function(){
+		return (win.scrollY + scr.availHeight >= this.element.offsetTop - this.threshold);
+	};
+
 	var setup = function(){
-		window.addEventListener('scroll', function(event){
+		win.addEventListener('scroll', execute);
 
-		});
-
+		if(win.scrollY == 0 ) execute();
 		setup = undef;
 	};
 
 	//return Sloth function
 	return function(params){
-		var elements = params.on,
-			threshold = params.threshold !== undef ? params.threshold : 100,
-			callback = params.callback;
+		if(params) {
+			var elements = params.on,
+				threshold = params.threshold !== undef ? params.threshold : 100,
+				callback = params.callback;
 
-		if(!elements) throw 'No elements passed';
-		if(!callback) throw 'No callback passed';
+			if(!elements) throw 'No elements passed';
+			if(!callback) throw 'No callback passed';
 
-		setup && setup();
+			if(elements instanceof NodeList){
+				var i = elements.length;
 
-		if(elements instanceof Array){
-			var i = elements.length;
-
-			while(i--) {
-				branches.push(new Branch(elements[i], threshold, callback))
+				while(i--) {
+					branches.push(new Branch(elements[i], threshold, callback))
+				}
+			}else {
+				branches.push(new Branch(elements, threshold, callback))
 			}
-		}else {
-			branches.push(new Branch(elements, threshold, callback))
+
+			setup && setup();
+		} else{
+			throw 'Gimme some data';
 		}
+
 	}
 });
 
